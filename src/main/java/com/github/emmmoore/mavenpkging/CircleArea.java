30a3f381430e3906b644bc48cff183b2ebde636f
elanor <elanor.m.moore@gmail.com>
2020-10-06 22:12:11 -0700

    removed zero width spaces

--- src/main/java/com/github/emmmoore/mavenpkging/CircleArea.java
+++ src/main/java/com/github/emmmoore/mavenpkging/CircleArea.java
@@ -1,26 +1,24 @@
 package com.github.emmmoore.mavenpkging;
-​
+
 public class App {
-​
-		public double radius;
-		public App(){
-			radius = 5.0;
-		}
-		public App(double radiusR){
-			this.radius = radiusR;
-		}
-		public double getRadius(){
-			return this.radius;
-		}
-		public void setRadius(double radiusR){
-			this.radius = radiusR;
-		}
-		public double getCircleArea(){
-			return (Math.PI*radius*radius);
-		}
-​
-		public static void main(String[] args){
-			App a = new App(5);
-			System.out.println("Circle with Radius = 5.0 has Area: " + a.getCircleArea());
-		}
+	public double radius;
+	public App() {
+		radius = 5.0;
 	}
+	public App(double radiusR) {
+		this.radius = radiusR;
+	}
+	public double getRadius() {
+		return this.radius;
+	}
+	public void setRadius(double radiusR) {
+		this.radius = radiusR;
+	}
+	public double getCircleArea() {
+		return (Math.PI * radius * radius);
+	}
+	public static void main(String[] args) {
+		App a = new App(5);
+		System.out.println("Circle with Radius = " + getRadius() + " has Area: " + getCircleArea());
+	}
+}
